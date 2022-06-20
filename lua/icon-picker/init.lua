df06db48fd79150275533cd0032f28b2c5efcbf1
ziontee113 <ziontee113@gmail.com>
2022-06-20 14:46:07 +0700

    added feedkeys for insert mode
    commit

--- lua/icon-picker/init.lua
+++ lua/icon-picker/init.lua
@@ -19,42 +19,64 @@ local function push_nerd_fonts()
 	end
 end
 
+local function insert_user_choice_normal(choice)
+	local split = vim.split(choice, " ")
+
+	--> without vim.schedule it won't exit to normal mode
+	vim.schedule(function()
+		vim.cmd("norm! i" .. split[1] .. "")
+	end)
+end
+local function insert_user_choice_insert(choice)
+	if choice then
+		local split = vim.split(choice, " ")
+
+		vim.cmd("norm! a" .. split[1])
+		local key = vim.api.nvim_replace_termcodes("<Right>", true, true, true)
+		vim.api.nvim_feedkeys(key, "i", false)
+	end
+end
+
+local function custom_ui_select(items, prompt, callback)
+	vim.ui.select(items, {
+		prompt = prompt,
+	}, callback)
+end
+
+-- Commands for Normal Mode
 vim.api.nvim_create_user_command("PickEmoji", function()
 	item_list = {}
 	push_emojis()
-
-	vim.ui.select(item_list, {
-		prompt = "Emoji Picker",
-	}, function(choice)
-		if choice then
-			print("I chose " .. choice)
-		end
-	end)
+	custom_ui_select(item_list, "Pick an emoji", insert_user_choice_normal)
 end, {})
 vim.api.nvim_create_user_command("PickNerd", function()
 	item_list = {}
 	push_nerd_fonts()
-
-	vim.ui.select(item_list, {
-		prompt = "Nerd Font Icon Picker Picker",
-	}, function(choice)
-		if choice then
-			print("I chose " .. choice)
-		end
-	end)
+	custom_ui_select(item_list, "Pick a Nerd Font", insert_user_choice_normal)
 end, {})
 vim.api.nvim_create_user_command("PickIcons", function()
 	item_list = {}
 	push_emojis()
 	push_nerd_fonts()
+	custom_ui_select(item_list, "Pick an Icon", insert_user_choice_normal)
+end, {})
 
-	vim.ui.select(item_list, {
-		prompt = "Emojis & Nerd Font Picker",
-	}, function(choice)
-		if choice then
-			print("I chose " .. choice)
-		end
-	end)
+-- Commands for Insert Mode
+vim.api.nvim_create_user_command("PickEmojiInsert", function()
+	item_list = {}
+	push_emojis()
+	custom_ui_select(item_list, "Pick an emoji", insert_user_choice_insert)
+end, {})
+vim.api.nvim_create_user_command("PickNerdInsert", function()
+	item_list = {}
+	push_nerd_fonts()
+	custom_ui_select(item_list, "Pick a Nerd Font", insert_user_choice_insert)
+end, {})
+vim.api.nvim_create_user_command("PickIconsInsert", function()
+	item_list = {}
+	push_emojis()
+	push_nerd_fonts()
+	custom_ui_select(item_list, "Pick an Icon", insert_user_choice_insert)
 end, {})
 
 return M
